5bc717d053d550445479eecbbe8ab92a70a174a2
hnprivv <huzaifanajmuddin707@gmail.com>
2026-07-01 03:35:27 +0500

    update page icon in Streamlit configuration

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from analyst import analyse, interpret
 
-st.set_page_config(page_title="Prism", page_icon="🔬", layout="wide")
+st.set_page_config(page_title="Prism", page_icon="icon.png", layout="wide")
 
 st.markdown("""
 <style>
